a2ae80ac7445c6704c0903580c5cf05eca698264
Oliver Völke <ovoelke@Olivers-MBP.fritz.box>
2021-02-25 23:02:49 +0100

    Add new Xcode project

---
+++ Xcode/Kids Paint/Kids Paint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kids Paint
+//
+//  Created by Oliver Völke on 25.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
